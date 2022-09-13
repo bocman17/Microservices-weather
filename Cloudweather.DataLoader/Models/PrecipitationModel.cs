24fca222f53f43a15c1d32ccbce74872ec4a82f4
Tomas Hejlik <tomas.hejlik@plus4u.net>
2022-09-14 00:54:51 +0200

    Build initial Data loader

---
+++ Cloudweather.DataLoader/Models/PrecipitationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cloudweather.DataLoader.Models
+{
+    internal class PrecipitationModel
+    {
+        public DateTime CreatedOn { get; set; }
+        public decimal AmountInches { get; set; }
+        public string WeatherType { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+}
